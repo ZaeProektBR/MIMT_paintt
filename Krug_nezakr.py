af86c0c9c18b84137d22f136aa59323264583b38
Iosif Stalin <80446233+ZaeProektBR@users.noreply.github.com>
2021-03-11 12:15:48 +0300

    Create Krug_nezakr.py

---
+++ Krug_nezakr.py
@@ -0,0 +1,15 @@
+width, height = 11, 11
+a, b = 5, 5
+r = 5
+EPSILON = 2.2
+
+map_ = [[' ' for x in range(width)] for y in range(height)]
+
+for y in range(height):
+    for x in range(width):
+        if abs((x-a)**2 + (y-b)**2 - r**2) < EPSILON**2:
+            map_[y][x] = '#'
+
+# print the map
+for line in map_:
+    print (' '.join(line))
